2ea2a0865b05c2b376e869025695745100e08022
Tassio Samuel <tassio.samuel72@gmail.com>
2023-02-06 22:03:35 -0300

    first commit

---
+++ media-aluno.py
@@ -0,0 +1,12 @@
+#entrada de dados
+nota1 = float(input("Nota : "))
+nota2 = float(input("Nota 2: "))
+nota3 = float(input("Nota 3: "))
+nota4 = float(input("Nota 4: "))
+
+#proocessamento
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+#saída de dados
+
+print(media)
