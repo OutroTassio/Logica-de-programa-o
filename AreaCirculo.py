f3809ed7e9ffa701ea563487a66b793356ca41a8
Tassio Samuel <tassio.samuel72@gmail.com>
2023-02-20 18:35:48 -0300

    Calcular area de um circulo

---
+++ AreaCirculo.py
@@ -0,0 +1,9 @@
+###
+#  Fazer um programa para ler o valor de "r" do raio de um circulo, e depois mostrar
+#  p valor da área do circulo com três casas decimais. A fórmula da área é : area = PI * r²###
+from math import pi
+r = float(input('Digite o valor do ráio: '))
+
+area = pi * r ** 2
+
+print(f'A área do circulo é {area:,.3f}')
