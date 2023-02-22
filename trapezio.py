73c5759c989b39c902212c07960332e3c2f8dd7c
Tassio Samuel <tassio.samuel72@gmail.com>
2023-02-21 21:54:26 -0300

    Algoritimo que calcula a area de um trapézio

---
+++ trapezio.py
@@ -0,0 +1,13 @@
+###
+# Fazer um programa que leia três medidas b, B e h. Em seguida, calcular e mostrar (Com quatro casas decimais.
+# ###
+
+
+b = float(input('Valor da base menor: '))
+B = float(input('Valor da base menor: '))
+h = float(input('Valor da altura: '))
+
+area = (b + B) / 2 * h
+
+print(f'A área do trapézio é {area:.4f}')
+
